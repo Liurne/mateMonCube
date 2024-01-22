008e5848deb78b0395bfbdd025ee64f8948b3afc
Jules Coquard <jcoquard@z4r3p6.42lyon.fr>
2024-01-22 18:21:33 +0100

    trad casi fini

--- src/display/raycast.c
+++ src/display/raycast.c
@@ -6,7 +6,7 @@
 /*   By: jcoquard <jcoquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:55:13 by jcoquard          #+#    #+#             */
-/*   Updated: 2024/01/17 17:53:08 by jcoquard         ###   ########.fr       */
+/*   Updated: 2024/01/22 17:23:23 by jcoquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -187,12 +187,95 @@ Line Raycast::getLineForColor(const unsigned int &x,const unsigned int wW,const 
 
 void	raycast(t_data *cub)
 {
-	int i;
+	int		i;
+	int		collision;
+	int		side;
+	double	distance;
+	double	camera;
+	double	direction_ray_x;
+	double	direction_ray_y;
+	double	range_next_x;
+	double	range_next_y;
+	double	next_x;
+	double	next_y;
+	int		step_x;
+	int		step_y;
+	int		map_x;
+	int		map_y;
+	int		line_h;
+	int		start_line;
+	int		end_line;
 
 	i = 0;
 	while (i < cub->win.w)
 	{
-		
+		collision = 0;
+		camera = 2 * i / (double)cub->win.w - 1;
+		direction_ray_x = cub->pl.dir_x + cub->pl.plan_x * camera;
+		direction_ray_y = cub->pl.dir_y + cub->pl.plan_y * camera;
+		//mapX = int(player.getEntity().x);
+		//mapY = int(player.getEntity().y);
+		range_next_x = sqrt(1 + (direction_ray_y * direction_ray_y) / (direction_ray_x * direction_ray_x));
+		range_next_y = sqrt(1 + (direction_ray_x * direction_ray_x) / (direction_ray_y * direction_ray_y));
+		map_x = (int)(cub->pl.x / map->tile_dim);
+		map_y = (int)(cub->pl.y / map->tile_dim);
+		if (direction_ray_x < 0)
+		{
+			step_x = -1;
+			next_x = (cub->pl.x / map->tile_dim - map_x) * range_next_x;
+		}
+		else
+		{
+			step_x = 1;
+			next_x = (map_x + 1 - cub->pl.x / map->tile_dim) * range_next_x;
+		}
+		if (direction_ray_y < 0)
+		{
+			step_y = -1;
+			next_y = (cub->pl.y / map->tile_dim - map_y) * range_next_y;
+		}
+		else
+		{
+			step_y = 1;
+			next_y = (map_y + 1 - cub->pl.y / map->tile_dim) * range_next_y;
+		}
+		while (!collision && map_x > 0 && map_x < cub->map.w && map_y > 0 && map_y < cub->map.h)
+		{
+			if (next_x < next_y)
+			{
+				next_x += range_next_x;
+				map_x += step_x;
+				side = 0;
+			}
+			else
+			{
+				next_y += range_next_y;
+				map_y += step_y;
+				side = 1;
+			}
+			if (get_tile(&(cub->map), map_x, map_y) != 0)
+				collision = true;
+		}
+//		if(side == 0)
+//			perpWallDist.push_back(prochainX - distanceProchainX);
+//		else
+//			perpWallDist.push_back(prochainY - distanceProchainY);
+		if (collision)
+		{
+			//correction de l'effet fish-eye
+			if (side == 0)
+				distance = fabs((map_x - (cub->pl.x / map->tile_dim) + (1 - step_x) / 2) / direction_ray_x);
+			else
+				distance = fabs((map_y - (cub->pl.y / map->tile_dim) + (1 - step_y) / 2) / direction_ray_y);
+			line_h = cub->win.h / distance;
+			start_line = -line_h / 2 + cub->win.h / 2;
+			end_line = line_h / 2 + cub->win.h / 2;
+			if(start_line < 0)
+				start_line = 0;
+			if (end_line >= cub->win.w)
+				end_line = cub->win.h - 1;
+			return Line(x,drawStart,x,drawEnd);
+		}
 		i++;
 	}
 }
